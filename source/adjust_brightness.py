3df7b577d909efd9d4fb6f96dde7ba68d82a9f87
Grace Hollands <grace.j.hollands@gmail.com>
2025-03-24 20:01:29 -0400

    MAC address test

---
+++ source/adjust_brightness.py
@@ -0,0 +1,9 @@
+def getmac(interface):
+	try:
+		mac = open('/sys/class/net/'+interface+'/address').readline()
+	except:
+		mac = "00:00:00:00:00:00"
+	return mac[0:17]
+	
+mac = getmac(wlan01).replace(":", "")
+print(mac[-4:])
